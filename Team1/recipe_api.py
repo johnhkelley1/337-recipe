a8f78f1e869a269af785cb0486a935aaa5c86ff0
John Kelley <John@dhcp-10-105-140-137.wireless.northwestern.private>
2016-03-12 17:07:14 -0600

    created modules

--- Team1/recipe_api.py
+++ Team1/recipe_api.py
@@ -1,5 +1,14 @@
 '''Version 0.1'''
 
+from modules import ingredients
+from modules import tools
+from modules import methods
+
+import requests
+from bs4 import BeautifulSoup
+
+RECIPE_HTML = ""
+
 def autograder(url):
     '''Accepts the URL for a recipe, and returns a dictionary of the
     parsed results in the correct format. See project sheet for
@@ -7,3 +16,15 @@ def autograder(url):
     # your code here
 
     return results
+
+def getRecipe(url):
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	RECIPE_HTML = soup
+
+	#ingredients.get(RECIPE_HTML)
+	#tools.get(RECIPE_HTML)
+	#methods.get(RECIPE_HTML)
+
+
+getRecipe("http://allrecipes.com/Recipe/Easy-Garlic-Broiled-Chicken/")
